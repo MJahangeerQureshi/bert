27aaad0e3941461e37f07f38f7499a1cbd046c81
MJahangeerQureshi <jahangeerq135@gmail.com>
2019-03-25 09:27:43 +0500

    modified for upto 5o classes

--- run_classifier.py
+++ run_classifier.py
@@ -353,7 +353,8 @@ class ColaProcessor(DataProcessor):
 
   def get_labels(self):
     """See base class."""
-    return ["0", "1"]
+    return ["0","1","2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49","50"]
+
 
   def _create_examples(self, lines, set_type):
     """Creates examples for the training and dev sets."""
@@ -486,7 +487,7 @@ def file_based_convert_examples_to_features(
     if ex_index % 10000 == 0:
       tf.logging.info("Writing example %d of %d" % (ex_index, len(examples)))
 
-    feature = convert_single_example(ex_index, example, label_list,
+      feature = convert_single_example(ex_index, example, label_list,
                                      max_seq_length, tokenizer)
 
     def create_int_feature(values):
